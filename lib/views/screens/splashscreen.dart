aab010280e412a025c3d69b74e875034a4cd6dca
Iconia7 <mwanginewton239@gmail.com>
2024-11-01 01:52:32 +0300

    start page

---
+++ lib/views/screens/splashscreen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Splashscreen extends StatelessWidget {
+  const Splashscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/background.jpeg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(40),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/logo.jpeg"),
+                    radius: 70,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "WELCOME",
+                    style: GoogleFonts.notoSerif(
+                        fontSize: 35,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "TO",
+                    style: GoogleFonts.notoSerif(
+                        fontSize: 35,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "STREAMS APP",
+                    style: GoogleFonts.notoSerif(
+                        fontSize: 35,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const Spacer(),
+                  SizedBox(
+                    width: 150,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          Get.toNamed("/login");
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromARGB(255, 1, 152, 223),
+                            foregroundColor: Colors.white),
+                        child: Text("Get Started",
+                            style: GoogleFonts.notoSerif(
+                                fontSize: 17,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold))),
+                  ),
+                  const SizedBox(
+                    height: 85,
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
