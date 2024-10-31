7a5c08896bba6180a6116cf1ef20a1dc459b74d6
Iconia7 <mwanginewton239@gmail.com>
2024-11-01 01:49:13 +0300

    Fix:routes

--- lib/configs/routes.dart
+++ lib/configs/routes.dart
@@ -1,11 +1,12 @@
-
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:project/views/screens/home.dart';
 import 'package:project/views/screens/login.dart';
 import 'package:project/views/screens/signup.dart';
+import 'package:project/views/screens/splashscreen.dart';
 
 List<GetPage> routes = [
-  GetPage(name: "/", page:() => const Login()),
-  GetPage(name: "/signup", page:() => const Signup()),
-  GetPage(name: "/home", page:() => const Home()),
+  GetPage(name: "/", page: () => const Splashscreen()),
+  GetPage(name: "/login", page: () => Login()),
+  GetPage(name: "/signup", page: () => Signup()),
+  GetPage(name: "/home", page: () => const Home()),
 ];
