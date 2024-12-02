3f28480e50ed5ce67652687f4e4fe55ce647bb3b
Iconia7 <mwanginewton239@gmail.com>
2024-12-03 00:38:07 +0300

    Chore: add miniplayer

--- lib/views/widgets/miniplayer.dart
+++ lib/views/widgets/miniplayer.dart
@@ -1,62 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
+import 'package:get/get.dart';
+import 'package:project/controller/musiccontroller.dart';
 
 class MiniPlayer extends StatelessWidget {
-  final AudioPlayer audioPlayer;
-  final String songTitle;
-  final String songImage;
+  final MusicController musicController = Get.find();
 
-  const MiniPlayer({
-    super.key,
-    required this.audioPlayer,
-    required this.songTitle,
-    required this.songImage,
-  });
+  MiniPlayer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<bool>(
-      stream: audioPlayer.playingStream,
-      builder: (context, snapshot) {
-        final isPlaying = snapshot.data ?? false;
-        return isPlaying
-            ? Container(
-                height: 80,
-                decoration: BoxDecoration(
-                  color: Colors.grey[800],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(5),
-                        child: Image.asset(
-                          songImage,
-                          width: 50,
-                          height: 50,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      songTitle,
-                      style: const TextStyle(fontSize: 16, color: Colors.white),
-                    ),
-                    IconButton(
-                      icon: Icon(
-                        isPlaying ? Icons.pause_circle_filled : Icons.play_circle_filled,
-                      ),
-                      color: Colors.white,
-                      onPressed: () => audioPlayer.play(),
-                    ),
-                  ],
-                ),
-              )
-            : const SizedBox.shrink();
-      },
-    );
+    return Obx(() {
+      if (musicController.currentSongTitle.value.isEmpty) {
+        return SizedBox.shrink(); 
+      }
+
+      return Container(
+        color: Colors.blue,
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Image.network(
+              musicController.currentSongImage.value,
+              width: 50,
+              height: 50,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) {
+                return const Icon(Icons.music_note, size: 50, color: Colors.white);
+              },
+            ),
+            const SizedBox(width: 8),
+            Expanded(
+              child: Text(
+                musicController.currentSongTitle.value,
+                style: const TextStyle(color: Colors.white, fontSize: 16),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            
+            IconButton(
+              icon: Icon(
+                musicController.isPlaying.value
+                    ? Icons.pause
+                    : Icons.play_arrow,
+                color: Colors.white,
+              ),
+              onPressed: musicController.togglePlayPause,
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
